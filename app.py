d94b55ddd3997607cc6bd4015cdc05e5952390b7
pkg13224 <pkg13224@gmail.com>
2025-02-08 23:48:47 +0530

    cicd update

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, request, render_template
 import numpy as np
 import pandas as pd
 from src.pipeline.predict_pipeline import CustomData, PredictPipeline
+import os
 
 
 from sklearn.preprocessing import StandardScaler
@@ -47,4 +48,4 @@ def predict_datapoint():
 
 
 if __name__ =='__main__':
-    app.run(host = '0.0.0.0', port=8080)
+    app.run(host = '0.0.0.0', port=int(os.environ.get('PORT', 8080)))
